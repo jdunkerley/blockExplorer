5280d9a42dd9152adaec0aa24d4b458b0cbd254b
James Dunkerley <jdunkerley@scottlogic.com>
2015-10-01 19:17:53 +0100

    Tweaking...

--- app/components/nodeGraph/nodeGraph-directive.js
+++ app/components/nodeGraph/nodeGraph-directive.js
@@ -8,8 +8,9 @@
         scope: {
           nodes: '=',
           links: '=',
-          nodeDoubleClick: '&',
-          statusMessage: '&'
+          nodeClick: '&',
+          nodeMouseOver: '&',
+          nodeMouseOut: '&'
         },
         link: function(scope, element, attr) {
           // Create SVG Element (read width and height from attributes)
@@ -20,35 +21,34 @@
               height: attr.height || 800
             });
 
+          // Define Margins and Width, Height
+          var radius = 10;
           var margin = {top: 0, right: 0, bottom: 0, left: 0};
           var width = svg.attr('width') - margin.left - margin.right;
           var height = svg.attr('height') - margin.top - margin.bottom;
 
+          // Add background before anything else
           svg.append('rect')
               .attr({
                 class: 'chart bg',
                 width: width,
                 height: height
-              })
-              .style('cursor', 'move');
+              });
 
-          // Define Margins and Width, Height
-          var radius = 10;
-
-          var min_zoom = 0.0001;
-          var max_zoom = 7;
-          var zoom = d3.behavior.zoom().scaleExtent([min_zoom,max_zoom])
+          // Set up zoom
+          var zoom = d3.behavior.zoom()
+            .scaleExtent([attr.minZoom || 0.1, attr.maxZoom || 5]);
 
           // Define Chart
           var chart = svg.append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')scale(' + zoom.scale() + ')');
+            .attr('transform',
+            'translate(' + margin.left + ',' + margin.top + ')scale(' + zoom.scale() + ')');
 
-          zoom.on("zoom", function() {
-            chart.attr('transform', 'translate(' + d3.event.translate + ')scale(' + zoom.scale() + ')');
+          zoom.on('zoom', function() {
+            chart.attr('transform',
+              'translate(' + d3.event.translate + ')scale(' + zoom.scale() + ')');
           });
-
           svg.call(zoom);
-
 
           var force = d3.layout.force()
             .size([width, height])
@@ -57,7 +57,7 @@
 
           // Create Render
           var firstRender = false;
-          scope.render = function(nodes, links, stMessage, nodeDblClick) {
+          scope.render = function(nodes, links, scope) {
             // Update
             var link = chart.selectAll('.link')
               .data(links);
@@ -73,14 +73,21 @@
               .attr('class', 'link');
             node.enter().append('circle')
               .attr('class', 'node')
-              .on('dblclick', function(d) {
-                if (nodeDblClick) { nodeDblClick({datum: d}); }
+              .on('click', function(d) {
+                if (scope.nodeClick) { scope.nodeClick({datum: d}); }
+              })
+              .on('mouseover', function(d) {
+                if (scope.nodeMouseOver) { scope.nodeMouseOver({datum: d}); }
+              })
+              .on('mouseout', function(d) {
+                if (scope.nodeMouseOut) { scope.nodeMouseOut({datum: d}); }
               });
 
             // Do Classing
             node
               .classed('expanded', function(d) { return d.expanded; })
-              .classed('terminal', function(d) { return !d.txId; });
+              .classed('terminal', function(d) { return !d.txId; })
+              .classed('initial', function(d) { return d.initial; });
 
             function tick() {
               node.attr({
@@ -100,19 +107,15 @@
 
             force.on('tick', tick);
             force.on('end', function() {
-              console.log('Done - ' + Date.now());
-              if (stMessage) {
-                  stMessage({message: 'Done'});
-              }
+              //console.log('Done - ' + Date.now());
             });
 
-            if (stMessage) { stMessage({message: 'Running Force Layout... ' + Date.now()}); }
             force.start();
           };
 
           // Wire Up Watch
           scope.$watch('links', function() {
-            scope.render(scope.nodes, scope.links, scope.statusMessage, scope.nodeDoubleClick);
+            scope.render(scope.nodes, scope.links, scope);
           }, true);
         }
       };
--- app/txTree/txTree-controller.js
+++ app/txTree/txTree-controller.js
@@ -6,136 +6,151 @@
     'blockExplorer.bitCoin',
     'blockExplorer.nodeGraph-directive'
   ])
-      .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/tx/:blockHash', {
-          templateUrl: 'txTree/txTree.html',
-          controller: 'TxTreeCtrl',
-          controllerAs: 'txTreeCtrl'
-        }).when('/tx/:blockHash/:txId', {
-          templateUrl: 'txTree/txTree.html',
-          controller: 'TxTreeCtrl',
-          controllerAs: 'txTreeCtrl'
-        });
-      }])
-      .controller('TxTreeCtrl', ['bitCoinService', '$routeParams', function (bitCoinService, $routeParams) {
-        var self = this;
-        self.blockHash = $routeParams.blockHash;
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.when('/tx/:blockHash', {
+        templateUrl: 'txTree/txTree.html',
+        controller: 'TxTreeCtrl',
+        controllerAs: 'txTreeCtrl'
+      }).when('/tx/:blockHash/:txId', {
+        templateUrl: 'txTree/txTree.html',
+        controller: 'TxTreeCtrl',
+        controllerAs: 'txTreeCtrl'
+      });
+    }])
+    .controller('TxTreeCtrl', ['bitCoinService', '$routeParams', function (bitCoinService, $routeParams) {
+      var self = this;
+      self.blockHash = $routeParams.blockHash;
 
-        self.txId = $routeParams.txId;
+      self.txId = $routeParams.txId;
 
-        self.nodes = [];
-        self.nodeMap = {};
-        self.links = [];
+      self.nodes = [];
+      self.nodeMap = {};
+      self.links = [];
 
-        self.currentStatus = 'Initialising ...';
-        self.statusMessage = function (message) {
-          self.currentStatus = message;
-        };
+      self.currentStatus = 'Initialising ...';
 
-        function loadBlockObject() {
-          self.currentStatus = 'Loading block object for hash ' + self.blockHash + ' ...';
-          bitCoinService.getBlock(self.blockHash)
-              .then(function (block) {
-                if (block.hash) {
-                  self.blockObject = block;
+      function loadBlockObject() {
+        self.currentStatus = 'Loading block object for hash ' + self.blockHash + ' ...';
+        bitCoinService.getBlock(self.blockHash)
+          .then(function (block) {
+            if (block.hash) {
+              self.blockObject = block;
 
-                  if (!self.txId) {
-                    self.txId = block.tx[0];
-                  }
-
-                  self.txIndex = self.blockObject.tx.indexOf(self.txId);
-                  if (self.txIndex > 0) {
-                    self.previousTxId = self.blockObject.tx[self.txIndex - 1];
-                  }
-                  if (self.txIndex < self.blockObject.tx.length - 1) {
-                    self.nextTxId = self.blockObject.tx[self.txIndex + 1];
-                  }
-
-                  loadTransaction();
-                } else {
-                  self.failed = true;
-                  self.currentStatus = 'Failed to load block ' + self.hash;
-                }
-              });
-        }
-
-        function loadTransaction() {
-          self.currentStatus = 'Loading transaction object for txid ' + self.txId + ' ...';
-          bitCoinService.getTransaction(self.txId)
-              .then(function (tx) {
-                if (!tx.txid) {
-                  self.failed = true;
-                  self.currentStatus = 'Failed to load transaction ' + self.txId;
-                  return;
-                }
-
-                self.rootTransaction = tx;
-                self.nodeMap[self.txId] = self.nodes.length;
-                self.nodes.push({txId: self.txId, expanded: false});
-                self.currentStatus = false;
-                //expandTransaction(self.txId);
-                  expandAllNodes(0);
-              });
-        }
-
-        function expandAllNodes(nodeIndex) {
-            if (nodeIndex >= self.nodes.length) {
-                return;
-            }
-
-            self.nodes[nodeIndex].expanded = true;
-            self.currentStatus = 'Expanding transaction ' + self.nodes[nodeIndex].txId;
-
-            bitCoinService.getTransaction(self.nodes[nodeIndex].txId)
-                .then(function(d) {
-                    expandTxObject(d);
-                    expandAllNodes(nodeIndex + 1);
-                });
-        }
-
-        function expandTxObject(txObject) {
-          if (txObject && txObject.vin) {
-            var refNodeIdx = self.nodeMap[txObject.txid];
-            var newNodes = [];
-            var newLinks = [];
-
-            for (var i = 0; i < txObject.vin.length; i++) {
-              var childId = txObject.vin[i].txid;
-              var childIdx = self.nodes.length + newNodes.length;
-              if (childId) {
-                self.nodeMap[childId] = childIdx;
+              if (!self.txId) {
+                self.txId = block.tx[0];
               }
 
-              newNodes.push({txId: childId, expanded: !childId});
-              newLinks.push({source: refNodeIdx, target: childIdx});
+              self.txIndex = self.blockObject.tx.indexOf(self.txId);
+              if (self.txIndex > 0) {
+                self.previousTxId = self.blockObject.tx[self.txIndex - 1];
+              }
+              if (self.txIndex < self.blockObject.tx.length - 1) {
+                self.nextTxId = self.blockObject.tx[self.txIndex + 1];
+              }
+
+              loadTransaction();
+            } else {
+              self.failed = true;
+              self.currentStatus = 'Failed to load block ' + self.hash;
+            }
+          });
+      }
+
+      function loadTransaction() {
+        self.currentStatus = 'Loading transaction object for txid ' + self.txId + ' ...';
+        bitCoinService.getTransaction(self.txId)
+          .then(function(tx) {
+            if (!tx.txid) {
+              self.failed = true;
+              self.currentStatus = 'Failed to load transaction ' + self.txId;
+              return;
             }
 
-            if (newNodes.length > 0) {
-              self.nodes = self.nodes.concat(newNodes);
-              self.links = self.links.concat(newLinks);
+            self.rootTransaction = tx;
+            self.nodeMap[self.txId] = self.nodes.length;
+            self.nodes.push({txId: self.txId, expanded: false, initial: true});
+            self.currentStatus = false;
+            expandTransaction(self.txId);//expandAllNodes(0);
+          });
+      }
+
+      function expandAllNodes(nodeIndex) {
+        if (nodeIndex >= self.nodes.length) {
+          return;
+        }
+
+        self.nodes[nodeIndex].expanded = true;
+        self.currentStatus = 'Expanding transaction ' + self.nodes[nodeIndex].txId;
+
+        bitCoinService.getTransaction(self.nodes[nodeIndex].txId)
+          .then(function (d) {
+            expandTxObject(d);
+            expandAllNodes(nodeIndex + 1);
+          });
+      }
+
+      function expandTxObject(txObject) {
+        if (txObject && txObject.vin) {
+          var refNodeIdx = self.nodeMap[txObject.txid];
+          var newNodes = [];
+          var newLinks = [];
+
+          for (var i = 0; i < txObject.vin.length; i++) {
+            var childId = txObject.vin[i].txid;
+            var childIdx = self.nodes.length + newNodes.length;
+            if (childId) {
+              self.nodeMap[childId] = childIdx;
             }
+
+            newNodes.push({txId: childId, expanded: !childId});
+            newLinks.push({source: refNodeIdx, target: childIdx});
           }
 
-          self.currentStatus = false;
-        }
-
-        function expandTransaction(parentTxId) {
-
-          var refNodeIdx = self.nodeMap[parentTxId];
-          if (refNodeIdx === null || self.nodes[refNodeIdx].expanded) {
-            return;
+          if (newNodes.length > 0) {
+            self.nodes = self.nodes.concat(newNodes);
+            self.links = self.links.concat(newLinks);
           }
-
-          self.nodes[refNodeIdx].expanded = true;
-          self.currentStatus = 'Expanding transaction ' + parentTxId;
-          bitCoinService.getTransaction(parentTxId)
-              .then(expandTxObject);
         }
 
-        self.expandNode = function (d) {
-          expandTransaction(d.txId);
-        };
+        self.currentStatus = false;
+      }
 
-        loadBlockObject();
-      }]);
+      function expandTransaction(parentTxId) {
+
+        var refNodeIdx = self.nodeMap[parentTxId];
+        if (refNodeIdx === null || self.nodes[refNodeIdx].expanded) {
+          return;
+        }
+
+        self.nodes[refNodeIdx].expanded = true;
+        self.currentStatus = 'Expanding transaction ' + parentTxId;
+        bitCoinService.getTransaction(parentTxId)
+          .then(expandTxObject);
+      }
+
+      self.expandNode = function(d) {
+        expandTransaction(d.txId);
+      };
+
+      self.expandAll = function() {
+        expandAllNodes(0);
+      };
+
+      self.mouseOver = function(d) {
+        self.hoveredTxId = d.txId;
+        bitCoinService.getTransaction(d.txId)
+          .then(function(txObj) {
+            if (self.hoveredTxId === txObj.txId) {
+              self.hoveredNode = txObj;
+            }
+          });
+      };
+
+      self.mouseOut = function(d) {
+        self.hoveredTxId = false;
+        self.hoveredNode = false;
+      };
+
+      loadBlockObject();
+    }]);
 })();
